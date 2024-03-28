14ff8e4785e7844ff9a12e06f819ecc631526f3b
hjsawiso <164585559+hjsawiso@users.noreply.github.com>
2024-03-28 08:39:29 +0000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,49 @@
+<script>
+	const normalPlatformList = ['ios', 'android', 'devtools']
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			// let self = this
+			// uni.getSystemInfo({
+			// 	success: res => {
+			// 		if(!normalPlatformList.includes(res.platform)){
+			// 			alert("请用手机端查看")
+			// 			while (true) {
+			// 				console.error('error')
+			// 			}
+			// 		}
+			// 	},
+			// 	fail(e) {
+			// 		console.error(e)
+			// 		wx.showToast({
+			// 			title: '获取设备信息失败',
+			// 			icon: 'none',
+			// 		})
+			// 	}
+			// })
+
+		},
+		
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+	@import url("static/css/base.scss");
+
+	table {
+		width: 100% !important;
+	}
+
+	// @font-face {
+	// 	font-family: "myfont";
+	// 	src: url('@/font/YouSheBiaoTiHei.ttf') format("truetype");
+	// }
+</style>
